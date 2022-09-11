ba948754660b9a456e896fdbc4883fe1abe26d1e
Rob Bos <raj.bos@gmail.com>
2022-09-11 23:51:57 +0200

    Adding loading repo info

--- .github/workflows/functions.ps1
+++ .github/workflows/functions.ps1
@@ -4,19 +4,11 @@ Param (
   $access_token = $env:GITHUB_TOKEN
 )
 
+. .\library.ps1
+
 $statusFile = "status.json"
 Write-Host "Got an access token with length of [$($access_token.Length)], running for [$($numberOfReposToDo)] repos"
 
-function SaveStatus {
-    Param (
-        $existingForks
-    )
-    if ("" -ne $env:CI) {
-        # We are running in CI, so let's pull before we overwrite the file
-        git pull --quiet | Out-Null
-    }
-    $existingForks | ConvertTo-Json | Out-File -FilePath $statusFile -Encoding UTF8
-}
 function GetForkedActionRepos {
 
     # if file exists, read it
@@ -288,7 +280,7 @@ function ForkActionRepos {
             }
         }
         else {
-            $abc = "fake for double check"
+            Write-Host "Fake message for double check"
         }
         $counter++ | Out-Null
     }
@@ -368,170 +360,6 @@ function EnableDependabot {
     return $false
 }
 
-function SplitUrl {
-    Param (
-        $url
-    )
-
-    # this fails when finding the repo name from this url
-    #if ($url.StartsWith("https://github.com/marketplace/actions")) {
-    #    return "";
-    #}
-
-    # split the url into the last 2 parts
-    $urlParts = $url.Split('/')
-    $repo = $urlParts[-1]
-    $owner = $urlParts[-2]
-    # return repo and org
-    return $owner, $repo
-}
-
-function SplitUrlLastPart {
-    Param (
-        $url
-    )
-
-    # this fails when finding the repo name from this url
-    #if ($url.StartsWith("https://github.com/marketplace/actions")) {
-    #    return "";
-    #}
-
-    # split the url into the last part
-    $urlParts = $url.Split('/')
-    $repo = $urlParts[-1]
-    # return repo
-    return $repo
-}
-
-function GetBasicAuthenticationHeader(){
-    $CredPair = "x:$access_token"
-    $EncodedCredentials = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($CredPair))
-    
-    return "Basic $EncodedCredentials";
-}
-
-function ApiCall {
-    Param (
-        $method,
-        $url,
-        $body,
-        $expected,
-        [int] $backOff = 5
-    )
-    $headers = @{
-        Authorization = GetBasicAuthenticationHeader
-    }
-    if ($null -ne $body) {
-        $headers.Add('Content-Type', 'application/json')
-    }
-    # prevent errors with starting slashes
-    if ($url.StartsWith("/")) {
-        $url = $url.Substring(1)
-    }
-    # auto prepend with api url
-    if (!$url.StartsWith('https://api.github.com/')) {
-        $url = "https://api.github.com/"+$url
-    }
-
-    try
-    {
-        #$response = Invoke-RestMethod -Method $method -Uri $url -Headers $headers -Body $body -ContentType 'application/json'
-        if ($method -eq "GET") {
-            $result = Invoke-WebRequest -Uri $url -Headers $headers -Method $method -ErrorVariable $errvar -ErrorAction Continue
-        }
-        else {
-            $result = Invoke-WebRequest -Uri $url -Headers $headers -Method $method -Body $body -ContentType 'application/json' -ErrorVariable $errvar -ErrorAction Continue
-        }
-
-        $response = $result.Content | ConvertFrom-Json
-        #Write-Host "Got this response: $($response | ConvertTo-Json)"
-        # todo: check and handle the rate limit headers
-        Write-Debug "  StatusCode: $($result.StatusCode)"
-        Write-Debug "  RateLimit-Limit: $($result.Headers["X-RateLimit-Limit"])"
-        Write-Debug "  RateLimit-Remaining: $($result.Headers["X-RateLimit-Remaining"])"
-        Write-Debug "  RateLimit-Reset: $($result.Headers["X-RateLimit-Reset"])"
-        Write-Debug "  RateLimit-Used: $($result.Headers["X-Ratelimit-used"])"
-        Write-Debug "  Retry-After: $($result.Headers["Retry-After"])"
-        
-        if ($result.Headers["Link"]) {
-            #Write-Host "Found pagination link: $($result.Headers["Link"])"
-            # load next link from header
-            $result.Headers["Link"].Split(',') | ForEach-Object {
-                # search for the 'next' link in this list
-                $link = $_.Split(';')[0].Trim()
-                if ($_.Split(';')[1].Contains("next")) {
-                    $nextUrl = $link.Substring(1, $link.Length - 2)
-                    # and get the results
-                    $nextResult = ApiCall -method $method -url $nextUrl -body $body -expected $expected
-                    $response += $nextResult
-                }
-            }            
-        }
-        
-        $rateLimitRemaining = $result.Headers["X-RateLimit-Remaining"]
-        $rateLimitReset = $result.Headers["X-RateLimit-Reset"]
-        if ($rateLimitRemaining -lt 10) {
-            Write-Host "Rate limit is low, waiting for [$rateLimitReset] ms before continuing"
-            # convert rateLimitReset from epoch to ms
-            $rateLimitReset = [DateTime]::FromBinary($rateLimitReset).ToUniversalTime()
-            $rateLimitReset = $rateLimitReset - [DateTime]::Now
-            Write-Host "Waiting [$rateLimitReset] for rate limit reset"
-            Start-Sleep -Milliseconds $rateLimitReset
-        }
-
-        if ($null -ne $expected) {
-            if ($result.StatusCode -ne $expected) {
-                Write-Host "  Expected status code [$expected] but got [$($result.StatusCode)]"
-                return $false
-            }
-            else {
-                return $true
-            }
-        }
-
-        return $response
-    }
-    catch
-    {
-        $messageData = $_.ErrorDetails.Message | ConvertFrom-Json
-        
-        if ($messageData.message -eq "was submitted too quickly") {
-            Write-Host "Rate limit exceeded, waiting for [$backOff] seconds before continuing"
-            Start-Sleep -Seconds $backOff
-            GetRateLimitInfo
-            return ApiCall -method $method -url $url -body $body -expected $expected -backOff ($backOff*2)
-        }
-        else {
-            Write-Host "Log message: $($messageData.message)"
-        }
-
-        if ($null -ne $expected)
-        {
-            Write-Host "Expected status code [$expected] but got [$($_.Exception.Response.StatusCode)] for [$url]"
-            if ($_.Exception.Response.StatusCode -eq $expected) {
-                # expected error
-                return $true
-            }
-            else {
-                return $false
-            }
-        }
-        else {
-            Write-Host "Error calling $url, status code [$($result.StatusCode)]"
-            Write-Host "MessageData: " $messageData 
-            Write-Host "Error: " $_
-            if ($result.Content.Length -gt 100) {
-                Write-Host "Content: " $result.Content.Substring(0, 100) + "..."
-            }
-            else {
-                Write-Host "Content: " $result.Content
-            }
-
-            throw
-        }
-    }
-}
-
 function ForkActionRepo {
     Param (
         $owner,
@@ -557,16 +385,6 @@ function ForkActionRepo {
     else {
         return $false
     }
-}
-
-function GetRateLimitInfo {
-    $url = "rate_limit"	
-    $response = ApiCall -method GET -url $url
-
-    #Write-Host "Ratelimit info: $($response.rate | ConvertTo-Json)"
-    Write-Host "Ratelimit info: $($response.rate | ConvertTo-Json)"
-    Write-Host " - GraphQL: $($response.resources.graphql | ConvertTo-Json)"
-    #Write-Host " - GraphQL: $($response | ConvertTo-Json)"
 }
 
 Write-Host "Got $($actions.Length) actions"
---
+++ .github/workflows/repoInfo.ps1
@@ -0,0 +1,117 @@
+Param (
+  $actions,
+  $numberOfReposToDo = 10,
+  $access_token = $env:GITHUB_TOKEN
+)
+
+. $PSScriptRoot/library.ps1
+
+Import-Module powershell-yaml -Force
+
+function GetActionType {
+    Param (
+        $owner,
+        $repo
+    )
+
+    # check repo for action.yml or action.yaml
+    $response = ""
+    $fileFound = ""
+    $actionType = ""
+    try {
+        $url = "/repos/$owner/$repo/contents/action.yml"
+        $response = ApiCall -method GET -url $url
+        $fileFound = "action.yml"
+    }
+    catch {
+        Write-Debug "No action.yml, checking for action.yaml"
+        try {
+            $url = "/repos/$owner/$repo/contents/action.yaml"
+            $response = ApiCall -method GET -url $url
+            $fileFound = "action.yaml"
+        }
+        catch {
+            # no files found
+            return "Unknown"
+        }
+    }
+
+    # load the file
+    $fileContent = ApiCall -method GET -url $response.download_url
+    Write-Debug "response: $($fileContent)"
+    $yaml = ConvertFrom-Yaml $fileContent
+    
+    # find line that says "
+    # runs:
+    #   using: "docker"
+    #   image: "Dockerfile""
+    # or:
+    # using: "node**"
+    
+    $using = $yaml.runs.using
+    $actionDockerType = ""
+    if ($using -eq "docker") {
+        $actionType = "Docker"
+        if ($yaml.runs.image -eq "Dockerfile") {
+            $actionDockerType = "Dockerfile"
+        }
+        else {
+            $actionDockerType = "Image"
+        }
+    }
+    else {
+        if ($using -like "node*") {
+            $actionType = "Node"
+        }
+        else {
+            $actionType = "Unknown"
+        }
+    }
+
+    return ($actionType, $fileFound, $actionDockerType)
+}
+
+$statusFile = "status.json"
+
+Write-Host "Got $($actions.Length) actions"
+GetRateLimitInfo
+
+# default variables
+$forkOrg = "actions-marketplace-validations"
+$status = $null
+if (Test-Path $statusFile) {
+    Write-Host "Using existing status file"
+    $status = Get-Content $statusFile | ConvertFrom-Json
+    
+    Write-Host "Found $($status.Count) existing repos in status file"
+}
+else {
+    Write-Error "Cannot find status file, halting execution"
+    return
+}
+
+$i = $status.Length
+$max = $status.Length + $numberOfReposToDo
+foreach ($action in $status) {
+
+    if ($i -ge $max) {
+        # do not run to long
+        Write-Host "Reached max number of repos to do, exiting: i:[$($i)], max:[$($max)], numberOfReposToDo:[$($numberOfReposToDo)]"
+        break
+    }
+
+    if (!(Get-Member -inputobject $action -name "actionType" -Membertype Properties)) {
+        ($actionTypeResult, $fileFoundResult, $actionDockerTypeResult) = GetActionType -owner $forkOrg -repo $action.name
+
+        $actionType = @{
+            actionType = $actionTypeResult 
+            fileFound = $fileFoundResult
+            actionDockerType = $actionDockerTypeResult
+        }
+
+        $action | Add-Member -Name actionType -Value $actionType -MemberType NoteProperty
+        $i++ | Out-Null
+    }
+}
+
+SaveStatus -existingForks $status
