11aa758922d076d0294fcc058600bc9ac4d3e15a
Rob Bos <raj.bos@gmail.com>
2022-09-18 12:34:33 +0200

    Only report once a day

--- local-run.ps1
+++ local-run.ps1
@@ -1,3 +1,3 @@
 
-.github/workflows/repoInfo.ps1  -actions $actions -numberofReposToDo 5
-#./.github/workflows/functions.ps1 -actions $actions -numberofReposToDo 5
+#./.github/workflows/repoInfo.ps1  -actions $actions -numberofReposToDo 5
+./.github/workflows/functions.ps1 -actions $actions -numberofReposToDo 5
