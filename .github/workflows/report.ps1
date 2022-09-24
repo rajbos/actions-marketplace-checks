085fd7a5f44db2b504266220fa9843c655f86011
Rob Bos <raj.bos@gmail.com>
2022-09-25 00:42:06 +0200

    Fix calculations

--- .github/workflows/report.ps1
+++ .github/workflows/report.ps1
@@ -233,8 +233,8 @@ function ReportAgeInsights {
     LogMessage "## Repo age"
     LogMessage "How recent where the repos updated? Determined by looking at the last updated date."
     LogMessage "Analyzed $repoInfo/$reposAnalyzed repos"
-
-    LogMessage "Oldest repository: $((Get-Date - $oldestRepo).TotalDays) days old"
+    $timeSpan = New-TimeSpan –Start $oldestRepo –End Get-Date
+    LogMessage "Oldest repository: $($timeSpan.Days) days old"
     LogMessage "Updated last month: $updatedLastMonth repos"
     LogMessage "Updated last 3 months: $updatedLastQuarter repos"
     LogMessage "Updated last 6 months: $updatedLast6Months repos"
