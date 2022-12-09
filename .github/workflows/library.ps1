5a6907c8bb5d41154a3e58f5b6a3eae3f9fbfb2c
Rob Bos <raj.bos@gmail.com>
2022-12-09 13:54:59 +0100

    Run for Dependabot

--- .github/workflows/library.ps1
+++ .github/workflows/library.ps1
@@ -482,7 +482,7 @@ function EnableDependabot {
 
     # enable dependabot if not enabled yet
     if ($null -eq $existingFork.dependabotEnabled) {
-        Write-Host "Enabling Dependabot for [$($existingFork.name)]"
+        Write-Debug "Enabling Dependabot for [$($existingFork.name)]"
         $url = "repos/$forkOrg/$($existingFork.name)/vulnerability-alerts"
         $status = ApiCall -method PUT -url $url -body $null -expected 204 -access_token $access_token_destination
         if ($status -eq $true) {
