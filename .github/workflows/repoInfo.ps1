ebbd9449d2ead2cbeecb3ae4bdcc9bf307ec9b99
Rob Bos <raj.bos@gmail.com>
2022-12-10 00:18:03 +0100

    Fix file setup

--- .github/workflows/repoInfo.ps1
+++ .github/workflows/repoInfo.ps1
@@ -408,7 +408,7 @@ function GetMoreInfo {
                             owner = $owner
                             repo = $repo
                         }
-                        $originalRepoDoesNotExists.Add($actionNoLongerExists)
+                        $originalRepoDoesNotExists.Add($actionNoLongerExists) | Out-Null
                         continue
                     }
 
